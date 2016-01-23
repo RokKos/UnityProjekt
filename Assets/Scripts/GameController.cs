47b83544c414aa8ea6e131f4eb51a3fa6866059b
RokKos <rocki5555@gmail.com>
2016-01-23 23:33:13 +0100

    Changed movin player so that now takes in consider the physics and added map

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,33 +8,39 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 	[SerializeField] GameObject igralec;
-	private float playerSpeed = 30.0f; 
+	private float playerSpeed = 10.0f; 
+	[SerializeField] Rigidbody rigitBody;
 	// Use this for initialization
 	void Awake() {
-		igralec.transform.position = new Vector3(0.0f, 2.6f, 0.0f); 
+		//igralec.transform.position = new Vector3(10.0f, 10.0f, 10.0f); 
 	}
 	
 	// Update is called once per frame
-	void Update () {	
+	void FixedUpdate () {	
 		if(Input.GetKey (KeyCode.RightArrow)){
 			//Debug.Log("here");
-			float newXcordinate = igralec.transform.position.x + playerSpeed * Time.deltaTime;
-			igralec.transform.position = new Vector3(newXcordinate, igralec.transform.position.y, igralec.transform.position.z);
+			float newXcordinate = + playerSpeed * Time.deltaTime;//igralec.transform.position.x  
+			//igralec.transform.position = new Vector3(newXcordinate, igralec.transform.position.y, igralec.transform.position.z);
+			rigitBody.MovePosition(igralec.transform.position + new Vector3(newXcordinate,0,0));
+
 		}
 		if(Input.GetKey (KeyCode.LeftArrow)){
 			//Debug.Log("her2");
-			float newXcordinate = igralec.transform.position.x - playerSpeed * Time.deltaTime;
-			igralec.transform.position = new Vector3(newXcordinate, igralec.transform.position.y, igralec.transform.position.z);
+			float newXcordinate = - playerSpeed * Time.deltaTime;//igralec.transform.position.x  
+			//igralec.transform.position = new Vector3(newXcordinate, igralec.transform.position.y, igralec.transform.position.z);
+			rigitBody.MovePosition(igralec.transform.position + new Vector3(newXcordinate,0,0));
 		}
 		if(Input.GetKey (KeyCode.UpArrow)){
 			//Debug.Log("here3");
-			float newZcordinate = igralec.transform.position.z + playerSpeed * Time.deltaTime;
-			igralec.transform.position = new Vector3(igralec.transform.position.x, igralec.transform.position.y, newZcordinate);
+			float newZcordinate = + playerSpeed * Time.deltaTime;//igralec.transform.position.z  
+			//igralec.transform.position = new Vector3(igralec.transform.position.x, igralec.transform.position.y, newZcordinate);
+			rigitBody.MovePosition(igralec.transform.position + new Vector3(0,0,newZcordinate));
 		}
 		if(Input.GetKey (KeyCode.DownArrow)){
 			//Debug.Log("her4");
-			float newZcordinate = igralec.transform.position.z - playerSpeed * Time.deltaTime;
-			igralec.transform.position = new Vector3(igralec.transform.position.x, igralec.transform.position.y, newZcordinate);
+			float newZcordinate = - playerSpeed * Time.deltaTime;//igralec.transform.position.z  
+			//igralec.transform.position = new Vector3(igralec.transform.position.x, igralec.transform.position.y, newZcordinate);
+			rigitBody.MovePosition(igralec.transform.position + new Vector3(0,0,newZcordinate));
 		}
 	}
 }
---
+++ Assets/Scripts/UIControler.cs
@@ -0,0 +1,49 @@
+﻿//Author: Rok Kos <rocki5555@gmail.com>
+//File: UIControler.cs
+//File path: /D/Documents/Unity/RUN/UnityProjekt/Assets/Scripts/UIControler.cs
+//Date: 23.1.2016
+//Description: Script for controling UI
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class UIControler : MonoBehaviour {
+
+	GameObject[] findGameObj;// = new List<string>(); 
+
+	// Use this for initialization
+	void Awake () {
+		findGameObj = GameObject.FindGameObjectsWithTag("Menu");
+		enableMenu("MainMenu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	/// <summary>
+	/// Change menu according to button
+	/// </summary>
+	public void ChangeMenu(Button selectedButton){
+		string regexButton = "Button";
+		//parse name of button to right menu
+		string strMenu = selectedButton.name.Remove(selectedButton.name.IndexOf(regexButton), regexButton.Length) + "Menu";
+		//call funtion to enable the menu
+		enableMenu(strMenu);
+	}
+	/// <summary>
+	/// Enable only one menu and disable everyone else
+	/// </summary>
+	void enableMenu(string nameOfMenu){
+		foreach (GameObject elem in findGameObj) {
+			if(elem.name == nameOfMenu){
+				elem.SetActive(true);
+			}else{
+				elem.SetActive(false);
+			}
+			
+		}
+	}
+}
