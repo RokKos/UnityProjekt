2d2f086da602100af2bad8008eb8ce15cf973ca3
RokKos <rocki5555@gmail.com>
2016-01-23 20:10:53 +0100

    Added funtionality to hurt player

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -9,21 +9,32 @@ using System.Collections;
 public class EnemyControl: MonoBehaviour {
 	NavMeshAgent agent;
 	GameObject igralec; //for postion of player
-	float angleOfDeath = 180.0f; //for what angle enemie rotate when dies
+	private float angleOfDeath = 180.0f; //for what angle enemie rotate when dies
+	private float timeAttack = 2.0f;
+	PlayerHealt playerHealt;
+	private bool uCanAttack = false;
 
 	// Use this for initialization
 	void Awake () {
 		//get agent
 		agent = GetComponent<NavMeshAgent>();
 		//find player in scene
-		igralec = GameObject.Find("Igralec"); 
+		igralec = GameObject.Find("Igralec");
+		playerHealt = igralec.GetComponent<PlayerHealt>();
 	}
 	
 	/// <summary>
 	/// enemy follow player, changing destination every frame
+	/// Attack player if in range
 	/// </summary>
 	void Update () {
 		agent.SetDestination(igralec.transform.position);
+		timeAttack -= Time.deltaTime;
+		//check if player is in range and if not dead and if enoug time alapsed
+		if(uCanAttack && timeAttack <= 0.0f && playerHealt.currHealth > 0 ){
+			playerHealt.TakeDamage(20);
+			timeAttack = 2.0f;
+		}
 	}
 
 	/// <summary>
@@ -43,6 +54,17 @@ public class EnemyControl: MonoBehaviour {
 		Debug.Log("Destroy");
 		*/
 		Destroy(gameObject, 0);
+	}
 
+	void OnTriggerEnter(Collider coll){
+		if(coll.gameObject == igralec){
+			uCanAttack = true;
+		}
+	}
+
+	void OnTriggerExit(Collider coll){
+		if(coll.gameObject == igralec){
+			uCanAttack = false;
+		}
 	}
 }
---
+++ Assets/Scripts/PlayerHealt.cs
@@ -0,0 +1,58 @@
+﻿//Author: Rok Kos <rocki5555@gmail.com>
+//File: PlayerHealt.cs
+//File path: /D/Documents/Unity/RUN/UnityProjekt/Assets/Scripts/PlayerHealt.cs
+//Date: 23.1.2016
+//Description: Script for player health
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHealt : MonoBehaviour {
+	private int startHealth = 100;
+	public int currHealth;
+	[SerializeField] Image udarecImage;
+	private float flashSpeed = 1.0f;
+	private Color flashColour = new Color(1f, 0f, 0f, 0.3f);
+	public bool damaged = false;
+	private bool isDeath = false;
+	
+	// Use this for initialization
+	void Awake () {
+		currHealth = startHealth;
+	}
+	
+	/// <summary>
+	/// shows if player is damgaded
+	/// and calls funtions for player deadt if so
+	/// </summary>
+	void Update () {
+		//if hit set image to that color	
+		if(damaged){
+			
+			udarecImage.color = flashColour;
+		}
+		//else degrade image
+		else{
+			udarecImage.color = Color.Lerp (udarecImage.color, Color.clear, flashSpeed * Time.deltaTime);
+		}
+		//reset to not duplicate effect
+		damaged = false;	
+	}
+	/// <summary>
+	/// tell player that u take damage
+	/// </summary>
+	public void TakeDamage(int damageValue){
+		damaged = true;
+		currHealth -= damageValue;
+		if(currHealth <= 0 && !isDeath){
+			Death();
+		}	
+	}
+	/// <summary>
+	/// kill the player and stop the game
+	/// </summary>
+	void Death(){
+		Time.timeScale = 0;
+		isDeath = true;
+	}
+}
