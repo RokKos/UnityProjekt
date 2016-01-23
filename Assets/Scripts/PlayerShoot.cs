c4df71a34d0902aa1d16a74763ec101538da4da8
RokKos <rocki5555@gmail.com>
2016-01-23 16:48:46 +0100

    text Assets

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -1,4 +1,10 @@
-﻿using UnityEngine;
+﻿//Author: Rok Kos <rocki5555@gmail.com>
+//File: PlayerShoot.cs
+//File path: /D/Documents/Unity/RUN/UnityProjekt/Assets/Scripts/PlayerShoot.cs
+//Date: 23.1.2016
+//Description: Script fo shooting
+
+using UnityEngine;
 using System.Collections;
 
 public class PlayerShoot : MonoBehaviour {
