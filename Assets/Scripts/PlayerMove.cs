14ddf5f06bc04b0f2b593e89e59bda939271dc39
brecbor <bor.brecelj@gmail.com>
2016-01-23 19:13:16 +0100

    Player movement added.

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using CnControls;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+    [SerializeField] GameObject igralec;
+    private float playerSpeed = 10.0f;
+    int x, y;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //premik igralca
+        Vector3 premik = igralec.transform.forward * CnInputManager.GetAxis("Vertical");
+        premik+=igralec.transform.right * CnInputManager.GetAxis("Horizontal");
+        igralec.transform.position = igralec.transform.position + premik;
+    }
+}
